4b96f20bac755becbed124d4bfa2347fabc5a5e0
Luan Bagio Compagnoni <luancmpg@gmail.com>
2024-05-28 22:18:49 -0300

    Init aside

---
+++ src/components/basics/aside/index.js
@@ -0,0 +1,16 @@
+function aside(){
+    return (
+        <aside className="h-screen">
+            <ul className={`
+            flex gap-5 items-center justify-center bg-purple-500
+             
+            `}>
+                <li>#1</li>
+                <li>#2</li>
+                <li>#3</li>
+            </ul>
+        </aside>
+    )
+}
+
+export default aside;

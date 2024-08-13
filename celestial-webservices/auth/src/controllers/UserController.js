e6a30dd93373e6129584c51bc5526a1b5efa38ca
Luan Bagio Compagnoni <luancmpg@gmail.com>
2024-08-13 16:01:37 -0300

    Config logger with Winston

--- celestial-webservices/auth/src/controllers/UserController.js
+++ celestial-webservices/auth/src/controllers/UserController.js
@@ -1,12 +1,15 @@
 import UserService from "../services/UserService.js";
+import logger from "../helpers/logger.js";
 
 class UserController{
     static async update(req,res,next){
         try {
             const user = req.body;
+            logger.info(`Update user: ${req.params.id}`)
             const result = await UserService.update(req.params.id, user);
             res.status(result.status).json(result.data)
         } catch (error) {
+            logger.error(`Update user error: ${error.message}`)
             next(error)
         }
     }
@@ -14,8 +17,10 @@ class UserController{
     static async delete(req,res,next){
         try {
             const result = await UserService.delete(req.params.id);
+            logger.info(`Delete user: ${req.params.id}`)
             res.status(result.status).json(result.data)
         } catch (error) {
+            logger.error(`Delete user error: ${error.message}`)
             console.log(error)
             next(error)
         }
@@ -24,8 +29,10 @@ class UserController{
     static async getById(req, res, next) {
         try {
             const result = await UserService.findById(req.params.id);
+            logger.info(`Get user by ID: ${req.params.id}`)
             res.status(result.status).json(result.data);
         } catch (error) {
+            logger.error(`Get user by ID error: ${error.message}`)
             next(error);
         }
     }
@@ -33,8 +40,10 @@ class UserController{
     static async list(req,res,next){
         try {
             const result = await UserService.list();
+            logger.info(`List user request`)
             res.status(result.status).json(result.data)
         } catch (error) {
+            logger.error(`List user error: ${error.message}`)
             next(error)
         }
     }

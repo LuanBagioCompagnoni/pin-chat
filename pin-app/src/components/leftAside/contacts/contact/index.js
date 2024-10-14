e922b6034b1b345d7a8220fa020f0460ca5866f0
LuanBagioCompagnoni <luancmpg@gmail.com>
2024-10-14 17:00:57 -0300

    Adjusted responsivity for mobile and adjusted profile page

--- pin-app/src/components/leftAside/contacts/contact/index.js
+++ pin-app/src/components/leftAside/contacts/contact/index.js
@@ -12,7 +12,9 @@ export default function Contact({ contactObject, onSelect, isSelected, isNotific
   };
 
   const formatName = () => {
-    const name = contact?.name;
+    const name = `${contact.name.slice(0, 20)}${contact.name.length > 20 ? '...' : ''}`;
+
+
     const prepositions = ['de', 'da', 'do', 'das', 'dos'];
     const nameParts = name.split(' ');
     const formatedName = nameParts.map(part => {
@@ -26,7 +28,7 @@ export default function Contact({ contactObject, onSelect, isSelected, isNotific
 
   return (
     <div
-      className={` hover:bg-[#e8e8e8] w-[98%] items-center justify-center shadow-gray-400 shadow-sm rounded-2xl mb-2 cursor-pointer 
+      className={` hover:bg-[#e8e8e8] w-[98%] h-16 items-center justify-center shadow-gray-400 shadow-sm rounded-2xl mb-2 cursor-pointer 
       ${isSelected ? 'bg-[#EDEDED]' : 'bg-[#F8F8F8]'}`
       }
       onClick={onSelect}
@@ -37,18 +39,18 @@ export default function Contact({ contactObject, onSelect, isSelected, isNotific
             {contact?.online ?
               <div className={'rounded-full bg-[#66BD71] absolute left-4 top-2.5 w-3.5 h-3.5'}></div> : <div className={'rounded-full bg-[#E57370] absolute left-4 top-2.5 w-3.5 h-3.5'}></div>
             }
-            <img className='rounded-full w-[60px] ml-4' src='https://cdn-icons-png.flaticon.com/512/4645/4645949.png' alt='UserImage'/>
+            <img className='rounded-full w-[50px] ml-4' src='https://cdn-icons-png.flaticon.com/512/4645/4645949.png' alt='UserImage'/>
           </div>
           <div className='content-center '>
             <h1 className="text-lg font-bold pl-4 text-[#2E2E2E]">{formatName()}</h1>
             <div className="flex">
               <div className="">
-                <h2 className="text-xs font-medium text-[#2E2E2E] px-4 p-0.5 break-words ">
+                <h2 className="text-xs font-medium text-[#8f8f8f] px-4 p-0.5 break-words ">
                   {lastMessage?.content?.slice(0, 35)}{lastMessage?.content?.length > 35 && '...'}
                 </h2>
               </div>
-              <h1 className="absolute right-2 top-2 text-sm font-medium text-[#2E2E2E]">{formatTime(lastMessage?.date)}</h1>
-              <div className={`${isNotification ? ' rounded-full bg-[#EF7D00] animate-pulse absolute right-2 bottom-5 w-5 h-5' : 'hidden'}`}></div>
+              <h1 className="absolute right-2  bottom-3 text-xs font-medium text-[#8f8f8f]">{formatTime(lastMessage?.date)}</h1>
+              <div className={`${isNotification ? ' rounded-full bg-[#EF7D00] animate-pulse absolute top-2 right-2  w-3.5 h-3.5' : 'hidden'}`}></div>
             </div>
           </div>
         </a>
--- pin-app/src/components/welcome/index.js
+++ pin-app/src/components/welcome/index.js
@@ -1,9 +1,12 @@
-
-export default function Welcome({user, className}) {
-  return (
-    <div className={`${className} flex flex-col bg-[#ffffff] justify-center`}>
-      <h1 className='text-7xl font-extrabold text-center mt-15 text-[#ef7d00]'><b>Bem vindo!</b></h1>
-      <img src='/welcome.png' alt='' />
-    </div>
-  );
+export default function Welcome({ className }) {
+    return (
+        <div className={`${className} flex flex-col bg-[#ffffff] justify-center h-screen overflow-hidden`}>
+            <h1 className="sm:text-7xl text-5xl font-extrabold text-center mt-15 text-[#ef7d00]"><b>Bem vindo!</b></h1>
+            <img
+                src="/welcome.png"
+                alt=""
+                className="max-w-full max-h-[80vh] object-contain"
+            />
+        </div>
+    );
 }
